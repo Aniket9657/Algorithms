ea6260f56702d63faeb40d187c4e255735f438c8
Aniket9657 <124755066+Aniket9657@users.noreply.github.com>
2025-08-28 06:17:27 +0530

    Basic Arrays And String

--- que1.py
+++ que1.py
@@ -1,8 +1,25 @@
-#Reverse Array using slicing
+# #Reverse Array using slicing
 
 arr =[1,3,45,5]
-def reverse_arr(arr):
-    return arr[::-1]
+# def reverse_arr(arr):
+#     return arr[::-1]
 
 
-print(reverse_arr(arr))
+# print(reverse_arr(arr))
+
+#Using list function 
+
+# l =list(arr)
+# l.reverse()
+# print(l)
+
+
+s="Holycow"
+print(s)
+l=list(s)
+
+print(l)
+
+l.reverse()
+print(l)
+print("".join(l))
