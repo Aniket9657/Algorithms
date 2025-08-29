d5146ed63156b1099ebc6d96cd5560e3a76df48b
Aniket9657 <124755066+Aniket9657@users.noreply.github.com>
2025-08-29 20:13:49 +0530

    List practice

---
+++ que3.py
@@ -0,0 +1,11 @@
+# Creste a python list 
+mylist =[10,6,7,3,5,7,8,23,56,1]
+print(mylist)
+
+mylist2= ["Ravi", "Kishor", "Mahesh","Ranjeet","Samuel"]
+print(mylist2)
+
+mylist3 = [ 4.3,24.0,6.6,4.4]
+print(mylist3)
+
+
