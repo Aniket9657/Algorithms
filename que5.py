9cfa27ddd573b0a12ed589033fc76fe73c337e29
Aniket9657 <124755066+Aniket9657@users.noreply.github.com>
2025-08-29 21:04:15 +0530

    List manipulation

--- que5.py
+++ que5.py
@@ -1,12 +1,31 @@
 mylist2= ["Ravi", "Kishor", "Mahesh","Ranjeet","Samuel"]
 print(mylist2)
 
-# #Delete multiple elements
+#Delete multiple elements
 # del mylist2[2:4]
 # print("Updated List \n",mylist2)
 
-# #Negative indexing 
+#Negative indexing 
 
 # print(mylist2[-1])
-for i in range (len( mylist2)) :
-  print(mylist2[i])
+# for i in range (len( mylist2)) :
+#   print(mylist2[i])
+
+#slice 
+
+# print(mylist2[2:3])
+# print(mylist2[2:])
+
+# for i in mylist2:
+#     print(i)
+
+
+# add elements using append 
+mylist2.append("Joker")
+
+print("Updated list \n",mylist2)
+
+# add element add specific index 
+mylist2.insert(2,"Ben10")
+
+print(" Update list \n",mylist2)
