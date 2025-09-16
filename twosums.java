bf8bb203ddbf8c0ea2f82322fe79990974271e14
Aniket9657 <124755066+Aniket9657@users.noreply.github.com>
2025-09-16 22:09:25 +0530

    Answer

--- twosums.java
+++ twosums.java
@@ -1,32 +1,26 @@
-class twosums
- {
-    public static void main(String[]args)
-    {
-        int arr [] ={0,2,2,7,9} ;
+class twosums {
+    public static void main(String[] args) {
+        int arr[] = {0, 2, 2, 7, 9};
         int target = 4;
 
-        ts(arr,target);
-    }
+        int[] result = ts(arr, target);
 
-
-    public static int[] ts(int [] arr,int target)
-    {
-        int [] fsum;
-        for (int i =0; i<arr.length-1;i++)
-        {
-            for (int j =i++;j<arr.length-1;j++)
-            {
-                int sum =i+j;
-                if (sum == target)
-                {
-                    fsum = {i,j};
-                    
-                }
-                 return fsum;
-               
-            }
-            
+        if (result != null) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No two numbers sum to target");
         }
     }
 
+    public static int[] ts(int[] arr, int target) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int sum = arr[i] + arr[j];
+                if (sum == target) {
+                    return new int[]{i, j};  // return indices
+                }
+            }
+        }
+        return null;  // if no pair is found
+    }
 }
